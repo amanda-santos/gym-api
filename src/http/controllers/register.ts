7ffbdd32f49e3b3a6fac0f1f5695e330d82e659d
amanda-santos <amandasf0200@gmail.com>
2023-03-15 18:38:25 -0300

    chore: 🥅 improve error handling

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -3,6 +3,7 @@ import { z } from 'zod'
 
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
 import { RegisterUseCase } from '@/use-cases/register'
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -22,8 +23,12 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
       email,
       password,
     })
-  } catch (err) {
-    return reply.status(409).send()
+  } catch (error) {
+    if (error instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({ message: error.message })
+    }
+
+    throw error
   }
 
   return reply.status(201).send()
